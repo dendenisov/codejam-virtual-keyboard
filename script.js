3781222cfe5a8c815a436d244f8b7a8e57ef1b51
Denis <MR.DENISOV1995@yandex.ru>
2020-03-11 14:14:06 +0300

    init: start JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const keyboard = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 97, 115, 100, 102, 103, 104, 
+    106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
+
+// document.onkeypress = function (event) {
+//     keyboard.push(event.charCode);
+//     console.log(keyboard);
+// }
+
+function init() {
+    let out = '';
+    for (let i=0; i<keyboard.length; i++) {
+        out += '<div class="key" >'+String.fromCharCode(keyboard[i])+'</div>';
+    }
+    document.querySelector('#keyboard').innerHTML = out;
+}
+
+init();
